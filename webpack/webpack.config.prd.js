9ad437c17b54b9ebac7b44365c83785aa85e0ebb
Kasper Chen <neko12377@gmail.com>
2020-11-08 18:44:22 +0800

    <refactor>(webpack): alter webpack configuration

--- webpack/webpack.config.prd.js
+++ webpack/webpack.config.prd.js
@@ -14,7 +14,7 @@ const config = {
         publicPath: "./",
     },
     resolve: {
-        extensions: [".tsx", ".js"],
+        extensions: [".tsx?", ".js"],
     },
     module: {
         rules: [
